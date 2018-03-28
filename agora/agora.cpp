b461786174407fb951edaebec8c3de4f91efc164
Marios Papachristou <papachristoumarios@gmail.com>
2018-03-28 13:45:30 +0300

    support for large nums

--- agora/agora.cpp
+++ agora/agora.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <vector>
 #include <cstdio>
+typedef unsigned long long ull;
 using namespace std;
-vector<int> a;
+vector<ull> a;
 int N, x;
 
-int gcd(int a, int b)
+ull gcd(ull a, ull b)
 {
     if (a == 0) return b;
     else return gcd(b%a, a);
 }
 
-int lcm(int a, int b) { return (a * b) / gcd(a,b); }
+ull lcm(ull a, ull b) { return (a * b) / gcd(a,b); }
 
-void printvec(vector<int> &v) {
+void printvec(vector<ull> &v) {
   for (unsigned int i = 0; i < v.size(); i++) cout << v[i] << " ";
   cout << endl;
 }
@@ -21,17 +22,19 @@ void printvec(vector<int> &v) {
 int main(int argc, char **argv) {
   FILE *fptr = fopen(argv[1], "r");
   a.push_back(1);
-  if (fptr) {
-    fscanf(fptr, "%d", &N);
-    for (int i = 0; i < N; i++) {
-      fscanf(fptr, "%d", &x);
-      a.push_back( x );
-    }
-  }
-  a.push_back(1);
+  if (!fptr) return -1;
 
-  vector<int> left_lcm( N + 2,  1);
-  vector<int> right_lcm( N + 2, 1);
+  fscanf(fptr, "%d", &N);
+  for (int i = 0; i < N; i++) {
+    fscanf(fptr, "%d", &x);
+    a.push_back( (ull) x );
+  }
+
+  a.push_back(1);
+  fclose(fptr);
+
+  vector<ull> left_lcm( N + 2,  1);
+  vector<ull> right_lcm( N + 2, 1);
 
   for ( int i = 1; i <= N + 1; i++) {
     left_lcm[i] = lcm(left_lcm[i-1], a[i]);
@@ -42,8 +45,8 @@ int main(int argc, char **argv) {
   }
 
   int min_index = -1;
-  int minimum = left_lcm[N];
-  int tmp;
+  ull minimum = left_lcm[N];
+  ull tmp = -1;
 
   for ( int i = 1; i <= N; i ++ ) {
     tmp = lcm ( left_lcm [ i-1 ] , right_lcm [ i+1 ]);
@@ -53,5 +56,5 @@ int main(int argc, char **argv) {
     }
   }
 
-  cout << minimum << " " << ( min_index + 1 ) << endl;
+  cout << minimum << " " << ( min_index == -1 ? 0 : min_index  ) << endl;
 }
