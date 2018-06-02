fdcc0225901c4ea3a333284fa3052225cf06b2b6
jim113 <37421143+jim113@users.noreply.github.com>
2018-06-03 00:13:15 +0300

    Need to add input from file
    Works for 2 first cases in form: play([team(cambodia, 1, 3, 7),team(vanuatu, 1, 2, 6),team(scotland, 2, 10, 10),team(chad, 2, 15, 7)],X).

--- mundial/mundial.pl
+++ mundial/mundial.pl
@@ -10,9 +10,22 @@ pushFront(Item,List,[Item|List]).
 sign(A,B):- C is A - B, C>0.
 
 getname(team(R, _, _,_), R).
+getname([team(R, _, _,_)], R).
 getmatches(team(_, R, _, _), R).
 getebale(team(_, _, R, _), R).
+getebale([team(_, _, R, _)], R).
 getefage(team(_, _, _, R), R).
+getefage([team(_, _, _, R)], R).
+
+getnameniki(team(R, _, _,_), R).
+getebaleniki(team(_, _, R, _), R).
+getefageniki(team(_, _, _, R), R).
+
+getlist([],Res):- Res=[].
+getlist([H|T],Res):-
+	(nonvar(H),Res=H,!;
+	getlist(T,Res)).
+
 
 team(monaco, 2, 10, 2).
 team(andorra, 2, 6, 4).
@@ -34,7 +47,9 @@ category(L,Losers,Winners,Res1,Res2):-
 categorize(L,Loser,Winner):-
 	category(L,[],[],Loser,Winner),!.
 
-valid(HW,HL,M):-
+	
+
+valid(HW,HL,M,Refr):-
 	getebale(HW,Ebale),
 	getefage(HL,Efage),
 	getebale(HL,Scor),
@@ -42,35 +57,62 @@ valid(HW,HL,M):-
 	(sign(Ebale,Efage), sign(Scor1,Scor), 
 	getname(HW,Nikitis), 	
 	getname(HL,Xamenos), 
-	M = match(Nikitis,Xamenos,Efage,Scor),!;
+	M = match(Nikitis,Xamenos,Efage,Scor),
+	New_ebale is Ebale - Efage,
+	New_efage is Scor1 - Scor,
+	getmatches(HW, R),
+	New_matches is R - 1,
+	Refr = team(Nikitis,New_matches,New_ebale,New_efage),
+	!;
 	M = [],!).
 
-find_valid_matcharisma([],[],Head,Res):- writeln(Res).
-find_valid_matcharisma(Loser,Winner,Head,Res):- %kefali listas loser
+find_valid_matcharisma([],[],Head,Res,U,New_winners):- 
+	%U=Res,
+	%writeln(New_winners),
+	categorize(New_winners,Loser,Winner),
+	head(Loser,HL),
+	(isEqual(Winner,[]),
+	head(Loser,H),
+	taill(Loser,T),
+	getname(H,Nikitis),
+	getname(T,Xamenos),
+	getebale(H,Ebale),
+	getefage(H,Efage),
+	getefage(T,Scor),
+	(isEqual(Scor,Ebale),	
+	M = match(Nikitis,Xamenos,Ebale,Efage),
+	append(Res,[M],Res1),
+	U = Res1,!;
+	!); 
+	%U=Res,!;
+	%writeln(Loser),
+	find_valid_matcharisma(Loser,Winner,HL,[],U,[])
+	
+	).
+
+
+find_valid_matcharisma([],A,Head,Res,U,New_winners):- !.
+find_valid_matcharisma(Loser,Winner,Head,Res,U,New_winners):- %kefali listas loser
 	head(Loser,HL),
 	head(Winner,HW),
-	valid(HW,HL,M),
+	valid(HW,HL,M,Refr),
 	taill(Loser,TL),
-	taill(Winner,TW),
-	( isEqual(M,[]),
-	append(TL,[HL],Loser1),
-	head(Loser1,New_l),
-	(isNotEqual(Head,New_l),
-	find_valid_matcharisma(Loser1,Winner,Head,Res);
-	isEqual(Head,New_l),!);
+	taill(Winner,TW),	
+	(isNotEqual(M,[]), 
+	append(Res,[M],Res1),
 	head(TL,Hea),
-	append(Res,M,Res1),
-	find_valid_matcharisma(TL,TW,Hea,Res1) ).
+	append(New_winners,[Refr],New_winners1),
+	find_valid_matcharisma(TL,TW,Hea,Res1,U,New_winners1);
+	append(TL,[HL],Loser11),
+	head(Loser11,HLL),
+	(isEqual(HLL,Head),!;
+	find_valid_matcharisma(Loser11,Winner,Head,Res,U,New_winners)		
+	)	
+	).
 	
-play(L):-
+	
+play(L,X):-
 	categorize(L,Loser,Winner),
 	head(Loser,HL),
-	find_valid_matcharisma(Loser,Winner,HL,[]).
-
-
-
-
-
-
-
-
+	findall(Z, find_valid_matcharisma(Loser,Winner,HL,[],Z,[]),Solutions),
+	getlist(Solutions,X).
