0cffca50e8ed34ba23216121ced4d5b5df46d40f
jim113 <37421143+jim113@users.noreply.github.com>
2018-06-03 00:38:02 +0300

    input added, correct for 8 teams, run 4 the rest.
    out of memory in last case

--- mundial/mundial.pl
+++ mundial/mundial.pl
@@ -85,8 +85,8 @@ find_valid_matcharisma([],[],Head,Res,U,New_winners):-
 	U = Res1,!;
 	!); 
 	%U=Res,!;
-	%writeln(Loser),
-	find_valid_matcharisma(Loser,Winner,HL,[],U,[])
+	%writeln(Res),
+	find_valid_matcharisma(Loser,Winner,HL,Res,U,[])
 	
 	).
 
@@ -116,3 +116,28 @@ play(L,X):-
 	head(Loser,HL),
 	findall(Z, find_valid_matcharisma(Loser,Winner,HL,[],Z,[]),Solutions),
 	getlist(Solutions,X).
+
+
+read_input(File, N, Teams) :-
+    open(File, read, Stream),
+    read_line_to_codes(Stream, Line),
+    atom_codes(Atom, Line),
+    atom_number(Atom, N),
+    read_lines(Stream, N, Teams).
+
+read_lines(Stream, N, Teams) :-
+    ( N == 0 -> Teams = []
+    ; N > 0  -> read_line(Stream, Team),
+                Nm1 is N-1,
+                read_lines(Stream, Nm1, RestTeams),
+                Teams = [Team | RestTeams]).
+
+read_line(Stream, team(Name, P, A, B)) :-
+    read_line_to_codes(Stream, Line),
+    atom_codes(Atom, Line),
+    atomic_list_concat([Name | Atoms], ' ', Atom),
+    maplist(atom_number, Atoms, [P, A, B]).
+
+mundial(File,Q):-
+	read_input(File, N, Teams),
+	play(Teams,Q).
